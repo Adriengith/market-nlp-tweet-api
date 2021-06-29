2a3c424a7d73a79fec589927171132333166a9c2
Adriengith <mygnyh@gmail.com>
2021-06-29 22:00:54 +0200

    1

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json
 import statistics
 from datetime import datetime
+import time
 #------------------------------------------------
 import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
@@ -11,7 +12,7 @@ from spacytextblob.spacytextblob import SpacyTextBlob
 app = FastAPI()
 
 
-
+# uvicorn api:app --reload
 
 
 @app.get("/")
@@ -19,10 +20,19 @@ async def root():
     return {"message": "ONLINE"}
 
 
-@app.get("/sentiments/{key_word}")
-async def sentiments(key_word):
+@app.get("/sentiments/{key_word}/{start_date}/{end_date}")
+async def sentiments(key_word, start_date, end_date):
     df = pd.read_csv("tweet.csv", sep=",")
     df_target = df[df['body'].str.contains(f"{key_word}")]
+    start_date = start_date.replace("-","/")
+    end_date = end_date.replace("-","/")
+    start_date = int(time.mktime(datetime.strptime(start_date, "%d/%m/%Y").timetuple()))
+    end_date = int(time.mktime(datetime.strptime(end_date, "%d/%m/%Y").timetuple()))
+
+
+    df_target = df_target[ (df_target["post_date"] >= start_date) & (df_target["post_date"] <= end_date)]
+
+
     result = df_target.to_json(orient="split")
     parsed = json.loads(result)
 
@@ -35,12 +45,13 @@ async def sentiments(key_word):
         nlp.add_pipe('spacytextblob')
         doc = nlp(element)
         list_polarity.append(doc._.polarity)
-        
-    round_polarity = round(statistics.mean(list_polarity),3)
-    #round_polarity = "HEROKU"
+    try:
+        round_polarity = round(statistics.mean(list_polarity),3)
+    except:
+        round_polarity = "None"
 
 
-    return f"Nombre de tweets trouvées pour '{key_word}' = {len(df_target)}",f"Moyenne des sentiments = {round_polarity}", parsed
+    return f"Nombre de tweets trouvées pour '{key_word}' = {len(df_target)}",f"Moyenne des sentiments = {round_polarity}",f"Total comments = A FAIRE",f"Total retweet = A FAIRE",f"Total like = A FAIRE", parsed
 
 
 if __name__ == "__main__":
