0fb5f47ae283fe47460fc948fbe1232b8400e643
Adriengith <mygnyh@gmail.com>
2021-06-29 17:39:19 +0200

    1

---
+++ api.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+import uvicorn
+import pandas as pd
+import json
+
+
+
+
+
+app = FastAPI()
+
+
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Simplon"}
+
+
+@app.get("/sentiments/{key_word}")
+async def test1(key_word):
+    df = pd.read_csv("tweet.csv", sep=",")
+    df_target = df[df['body'].str.contains(f"{key_word}")]
+    result = df_target.to_json(orient="split")
+    parsed = json.loads(result)
+    return f"Nombre de tweets trouvées pour '{key_word}' = {len(df_target)}", parsed
+
+@app.get("/test2")
+async def test2():
+    key_word = "test"
+    return {"id": f"{key_word}"}
+
+# @app.get("/students/id/{student_id}")
+# async def get_student_by_id(student_id):
+#     return {"student_id": student_id}
+
+
+# @app.get("/students/name/{lastname}")
+# async def get_student_by_name(lastname):
+#     return {"lastname": lastname}
+
+
+# @app.post("/students")
+# async def create_student(student: Student):
+#     student_id = str(COLLECTION_STUDENTS.insert_one(student.dict()).inserted_id)
+#     # TODO : Envoyer un mail de confirmation
+#     return {"student_id": student_id}
+
+
+# @app.put("/students")
+# async def update_student():
+#     return "Not yet implemented"
+
+
+@app.delete("/students/id/{id}")
+async def delete_student_by_id(id):
+    return {"id": id}
+
+
+# @app.delete("/students/name/{lastname}")
+# async def delete_student_by_name(lastname):
+#     return {"lastname": lastname}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
