528d46b4385bbf5839d5723f467a0f9f6b63a1fc
adrien.git <66469468+Adriengith@users.noreply.github.com>
2021-07-12 10:21:40 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 st.title("Hello guys")
