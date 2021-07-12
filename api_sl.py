f1ad7dc9a19ce76c1afe9549148c5dc2e871781c
adrien.git <66469468+Adriengith@users.noreply.github.com>
2021-07-12 09:58:56 +0200

    Add files via upload

--- api_sl.py
+++ api_sl.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 def main():
